30570fc95a023af6eb42c4840645d311739929dd
Bunyakhon <growmos@gmail.com>
2024-10-05 18:37:24 +0700

    finshed rent start search

---
+++ extension/extensions_drive_now.py
@@ -0,0 +1,21 @@
+import struct
+def get_next_id():
+    try:
+        with open("Doc/datas_drive_now.bin", "rb") as file:
+            records = []
+            record_size = struct.calcsize("i100s50s50s15s15sf")
+            
+            while True:
+                record_data = file.read(record_size)
+                if not record_data:
+                    break
+                record = struct.unpack("i100s50s50s15s15sf", record_data)
+                records.append(record)
+        if records:
+            max_id = max(record[0] for record in records)
+            return max_id + 1
+        else:
+            return 1
+    except Exception as e:
+        print(f"เกิดข้อผิดพลาดในการอ่านข้อมูล: {e}")
+        return 1
--- func_drive_now.py
+++ func_drive_now.py
@@ -1,9 +1,32 @@
 import datetime
+import struct
+import extension.extensions_drive_now as ex
+
 
 def search_data(menu_number):
     if menu_number == 1:
-        print("ค้นหาข้อมูล...")
-        return "ข้อมูลที่ค้นหา"  
+        try:
+            keep_going = True
+            while keep_going:
+                print("\t\t\tรายการเมนูค้นหาข้อมูล")
+                print("---------------------------------------------------------")
+                print("1. แสดงข้อมูลทั้งหมด")
+                print("2. ค้นหาเฉพาะ")
+                print("3. ออกจากเมนูการเช่ารถ")
+                print("---------------------------------------------------------")
+                search_data_menu = int(input("กรุณาเลือกรายการ : ")) 
+                
+                if search_data_menu == 1:
+                    pass
+                elif search_data_menu == 2:
+                    pass
+                elif search_data_menu == 3:
+                    print("ออกจากเมนูการค้นหาข้อมูล")
+                    keep_going = False
+                else:
+                    print("เมนูที่เลือกไม่ถูกต้อง")
+        except Exception as e:
+            print(f"เกิดข้อผิดพลาด: {e}")   
 
 def rent_car(menu_number):
     detail_car = {
@@ -14,7 +37,6 @@ def rent_car(menu_number):
         "Audi A5": "Coupe",
         "Hyundai Sonata": "Sedan"
     }
-
     if menu_number == 2:
         try:
             keep_going = True
@@ -32,27 +54,34 @@ def rent_car(menu_number):
                     print("รายการรถที่มีให้เช่า:")
                     for numer_id, (key, value) in enumerate(detail_car.items(), start=1):
                         print(f"{numer_id}. รถรุ่น: {key}, ประเภทรถ: {value}")
+                        
                 elif rent_car_menu == 2:
                     
                     date_rent = datetime.datetime.now()
-                    print("กรอกข้อมูลการเช่า...")
+                    print("...กรอกข้อมูลการเช่า...")
+                    id_cus = ex.get_next_id()
+                    print(f"ลำดับที่ (ID): {id_cus}")
                     cus_name = input("ชื่อ-นามสกุล : ")
                     car_model = input("รถรุ่น : ")
                     
-                    car_type = input("ประเภทรถ : ")
-                    if detail_car[car_model] != car_type:
-                        print("ประเภทของรถไม่ตรงกับรถรุ่นที่เลือก โปรดลองอีกครั้ง")
-                        continue
                     if car_model not in detail_car:
                         print("รถรุ่นที่เลือกไม่มีในรายการ โปรดลองอีกครั้ง")
                         continue
                     
+                    car_type = detail_car[car_model]
+                    print(f"ประเภทรถ: {car_type}")  
                     return_date_input = int(input("จำนวนวันที่เช่า : "))
                     return_date = date_rent + datetime.timedelta(days=return_date_input)
                     cost_rent = float(input("ค่าเช่าวันละ : "))
                     total_cost_rent = cost_rent * return_date_input
                     
+                    
+                    with open("Doc/datas_drive_now.bin", "ab") as file:
+                        data = struct.pack("i100s50s50s15s15sf", id_cus, cus_name.encode(), car_model.encode(), car_type.encode(), date_rent.strftime("%Y/%m/%d").encode(), return_date.strftime("%Y/%m/%d").encode(), total_cost_rent)
+                        file.write(data)
+                        
                     return {
+                        "ลำดับที่": id_cus,
                         "ชื่อ-นามสกุล": cus_name,
                         "รถรุ่น": car_model,
                         "ประเภทรถ": car_type,
@@ -68,8 +97,7 @@ def rent_car(menu_number):
                     print("เมนูที่เลือกไม่ถูกต้อง")
         except Exception as e:
             print(f"เกิดข้อผิดพลาด: {e}")
-            
-            
+                   
 def edit_data_customer(menu_number):
     if menu_number == 3:
         print("แก้ไขข้อมูลการจอง...")
