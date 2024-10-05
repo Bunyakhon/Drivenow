62c10c5dffcfdfa7b965e44681a3dfa934aeb6a5
Bunyakhon <growmos@gmail.com>
2024-10-05 13:28:06 +0700

    menu2not struck

--- drive_now.py
+++ drive_now.py
@@ -1,1 +1,58 @@
-print("Hello Drive Now !!!")
+import time
+import func_drive_now as fdn
+
+def main():
+    try:
+        keep_going = True
+        rental_info = {}  # ประกาศตัวแปร rental_info เพื่อใช้ในเมนูที่ 2
+        while keep_going:
+            print("==========================================================")
+            print("\t\t\tรายการเมนู")
+            print("==========================================================")
+            print("1. ค้นหาข้อมูล")
+            print("2. จองรถ")
+            print("3. แก้ไขข้อมูลการจองรถ")
+            print("4. ลบข้อมูล")
+            print("5. รายงานข้อมูล")
+            print("6. ปิดโปรแกรม")
+            print("==========================================================")
+
+            try:
+                number_menu = int(input("กรุณาใส่หมายเลข : "))
+                if number_menu == 1:
+                    output = fdn.search_data(number_menu)
+                    print(output)
+                    time.sleep(3)
+                elif number_menu == 2:
+                    rental_info = fdn.rent_car(number_menu)
+                    if rental_info:
+                        print("\nข้อมูลการเช่าที่ได้รับ:")
+                        for key, value in rental_info.items():
+                            print(f"{key}: {value}")
+                    time.sleep(3)
+                elif number_menu == 3:
+                    output = fdn.edit_data_customer(number_menu)
+                    print(output)
+                    time.sleep(3)
+                elif number_menu == 4:
+                    output = fdn.delet_data(number_menu)
+                    print(output)
+                    time.sleep(3)
+                elif number_menu == 5:
+                    output = fdn.report_data(number_menu)
+                    print(output)
+                    time.sleep(3)
+                elif number_menu == 6:
+                    print("จบการทำงาน")
+                    keep_going = False
+                else:
+                    print("ใส่หมายเลขเมนูให้ถูกต้อง")
+                    time.sleep(3)
+            except ValueError:
+                print("กรุณาใส่หมายเลขให้ถูกต้อง")
+                time.sleep(3)
+    except Exception as e:
+        print(f"เกิดข้อผิดพลาด: {e}")
+
+if __name__ == "__main__":
+    main()
---
+++ func_drive_now.py
@@ -0,0 +1,83 @@
+import datetime
+
+def search_data(menu_number):
+    if menu_number == 1:
+        print("ค้นหาข้อมูล...")
+        return "ข้อมูลที่ค้นหา"  
+
+def rent_car(menu_number):
+    detail_car = {
+        "Nissan Altima": "Sedan",
+        "Ford Mustang": "Coupe",
+        "Honda CR-V": "SUV",
+        "Toyota Corolla": "Sedan",
+        "Audi A5": "Coupe",
+        "Hyundai Sonata": "Sedan"
+    }
+
+    if menu_number == 2:
+        try:
+            keep_going = True
+            while keep_going:
+                print("\t\t\tรายการเมนูการเช่ารถ")
+                print("---------------------------------------------------------")
+                print("1. แสดงรายการรถที่มีให้เช่า")
+                print("2. ทำรายการเช่ารถ")
+                print("3. ออกจากเมนูการเช่ารถ")
+                print("---------------------------------------------------------")
+                rent_car_menu = int(input("กรุณาเลือกรายการ : "))
+                
+                if rent_car_menu == 1:
+                    
+                    print("รายการรถที่มีให้เช่า:")
+                    for numer_id, (key, value) in enumerate(detail_car.items(), start=1):
+                        print(f"{numer_id}. รถรุ่น: {key}, ประเภทรถ: {value}")
+                elif rent_car_menu == 2:
+                    
+                    date_rent = datetime.datetime.now()
+                    print("กรอกข้อมูลการเช่า...")
+                    cus_name = input("ชื่อ-นามสกุล : ")
+                    car_model = input("รถรุ่น : ")
+                    
+                    car_type = input("ประเภทรถ : ")
+                    if detail_car[car_model] != car_type:
+                        print("ประเภทของรถไม่ตรงกับรถรุ่นที่เลือก โปรดลองอีกครั้ง")
+                        continue
+                    if car_model not in detail_car:
+                        print("รถรุ่นที่เลือกไม่มีในรายการ โปรดลองอีกครั้ง")
+                        continue
+                    
+                    return_date_input = int(input("จำนวนวันที่เช่า : "))
+                    return_date = date_rent + datetime.timedelta(days=return_date_input)
+                    cost_rent = float(input("ค่าเช่าวันละ : "))
+                    total_cost_rent = cost_rent * return_date_input
+                    
+                    return {
+                        "ชื่อ-นามสกุล": cus_name,
+                        "รถรุ่น": car_model,
+                        "ประเภทรถ": car_type,
+                        "วันที่เช่า": date_rent.strftime("%Y/%m/%d"),
+                        "จำนวนวันที่เช่า": return_date_input,
+                        "จำนวนเงิน": total_cost_rent,
+                        "วันที่คืนรถ": return_date.strftime("%Y/%m/%d")  
+                    }
+                elif rent_car_menu == 3:
+                    print("ออกจากเมนูการเช่ารถ")
+                    keep_going = False
+                else:
+                    print("เมนูที่เลือกไม่ถูกต้อง")
+        except Exception as e:
+            print(f"เกิดข้อผิดพลาด: {e}")
+            
+            
+def edit_data_customer(menu_number):
+    if menu_number == 3:
+        print("แก้ไขข้อมูลการจอง...")
+        
+def delet_data(menu_number):
+    if menu_number == 4:
+        print("ลบข้อมูล...")
+        
+def report_data(menu_number):
+    if menu_number == 5:
+        print("รายงานข้อมูล...")
